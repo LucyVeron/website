fe33963e6857f3d1379d183db8f81d57490d7af3
lucy <lucymveron@gmail.com>
2017-02-21 16:13:18 +0100

    added image gallery template

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+
+import os
+
 from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-   return render_template('hometest.html')
+   return render_template('home.html')
 
-@app.route('/gallery.html')
-def gallery():
-   return render_template('gallerytest.html')
+@app.route('/gallery')
+def get_gallery():
+   image_names = os.listdir('./static/images/gallery')
+   print(image_names)
+   return render_template("gallery.html", image_names=image_names)
 
 if __name__ == '__index__':
    app.run(debug=True)
