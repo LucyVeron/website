c47fd53536df1c28bdc1add58e58df93badd8a02
lucy <lucymveron@gmail.com>
2017-03-03 12:50:05 +0100

    New bio page URL

--- server.py
+++ server.py
@@ -12,8 +12,13 @@ def index():
 
 @app.route('/gallery')
 def get_gallery():
-   image_names = os.listdir('./static/images/gallery')
-   return render_template("gallery.html", layout = "gallery", image_names=image_names)
+   gallery_image_names = os.listdir('./static/images/gallery')
+   return render_template("gallery.html", layout = "gallery", gallery_image_names=gallery_image_names)
+
+@app.route('/bio')
+def get_bio():
+   bio_image_names = os.listdir('./static/images/bio')
+   return render_template("bio.html", layout = "bio", bio_image_names=bio_image_names)
 
 if __name__ == '__index__':
    app.run(debug=True)
